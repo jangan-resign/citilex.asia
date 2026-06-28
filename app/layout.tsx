88e842f9947f611bb734a67ee69770b5cd717e55
jangan-resign <behagroup@gmail.com>
2026-06-28 14:26:01 +0700

    Update analytics tracking scripts in layout.tsx
    Removed Google Tag Manager and Google Analytics placeholders, and updated the Google Analytics script with a specific tracking ID.

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,47 +61,29 @@ export default function RootLayout({
       className={`${inter.variable} ${montserrat.variable}`}
     >
       <head>
-        {/* ==========================================================
-            ANALYTICS & CONVERSION TRACKING PLACEHOLDERS
-            Replace GTM-XXXXXXX and G-XXXXXXXXXX with actual keys in production.
-           ========================================================== */}
-
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];
-              w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),
-              dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXXX');
-            `,
-          }}
-        />
-
-        {/* Google Analytics */}
+   
+        {/* Google Ads Conversion Tracking */}
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18279242485"
         />
-
+        
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX');
+        
+              gtag('config', 'AW-18279242485');
             `,
           }}
         />
+  
       </head>
 
       <body
+        
         className="
           antialiased
           font-inter
@@ -112,22 +94,10 @@ export default function RootLayout({
         "
       >
 
-        {/* Google Tag Manager noscript */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
-            height="0"
-            width="0"
-            style={{
-              display: "none",
-              visibility: "hidden",
-            }}
-          />
-        </noscript>
-
         {children}
 
       </body>
+      
     </html>
   );
 }
