d401ff5c0a5c32c9e2ee47893f282bf5452610b7
jangan-resign <behagroup@gmail.com>
2026-07-31 20:12:07 +0700

    Update address format in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1019,7 +1019,8 @@ export default function LandingPage() {
             <MapPin className="mt-1 w-[15px] h-[15px] shrink-0 text-brand-primary" />
       
             <div className="text-sm md:text-base leading-relaxed">
-              <p>Bintaro Pesanggrahan Jakarta Selatan, Indonesia</p>
+              <p>Bintaro, Pesanggrahan</p>
+              <p>Jakarta Selatan, Indonesia</p>
             </div>
       
           </div>
